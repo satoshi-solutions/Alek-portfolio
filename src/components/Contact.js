be631c63f80e2e65d4a7292d137c0e9bda1c5139
satoshi-solutions <satoshi.bitcoin.solutions@gmail.com>
2025-04-11 09:44:24 -0400

    added contact info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ const Contact = () => (
     <div className="max-w-4xl mx-auto">
       <h2 className="text-3xl font-semibold text-white mb-4">Contact Me</h2>
       <p className="text-lg text-white mb-6">I'm open to collaborations and job opportunities. Feel free to get in touch! Thanks for visiting!</p>
-      <a href="https://mail.google.com/mail/?view=cm&fs=1&to=satoshi.bitcoin.solutions@gmail.com" className="bg-gradient-to-b from-[rgba(50,50,50,0.7)] via-gray-850 to-gray-900 text-blue-600 py-3 px-8 rounded-full text-lg hover:text-gray-100 transition duration-300">Email Me</a>
+      <a href="https://mail.google.com/mail/?view=cm&fs=1&to=acako999@gmail.com" className="bg-gradient-to-b from-[rgba(50,50,50,0.7)] via-gray-850 to-gray-900 text-blue-600 py-3 px-8 rounded-full text-lg hover:text-gray-100 transition duration-300">Email Me</a>
     </div>
   </section>
   
