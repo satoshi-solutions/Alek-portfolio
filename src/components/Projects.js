cf818f83dd82c4074a85d024957bf6fddf66cf66
satoshi-solutions <satoshi.bitcoin.solutions@gmail.com>
2025-04-11 09:50:12 -0400

    bnb

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -17,7 +17,7 @@ const Projects = () => {
     },
     {
       title: 'iVaCay Presale',
-      description: 'Presale Sites on BNB Chain.',
+      description: 'IVAC Presale Site On BNB.',
       image: '/images/project3.png', // Replace with your image path
       url: 'https://ivacay.io/', // Replace with your project URL
     },
